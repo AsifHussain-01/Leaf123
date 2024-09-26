c15cdda5f4661ad1a9c72e40c44776922ab9f8ed
unknown <hr.alphasquad01@gmail.com>
2024-09-26 15:57:02 +0500

    first

--- src/App.js
+++ src/App.js
@@ -1,47 +1,50 @@
 
 function App() {
   return (
-    <div className="pl-14 pr-14 max-w-full h-full w-max h-max mr-auto ml-auto gap-5 bg-[#e9ecef]">
-      <div className="pl-14 pr-14 w-full h-full w-max h-max items-center mr-auto ml-auto flex justify-center justify-between gap-5 ">
-      <img className="h-[50px] w-[50px] mt-5"src="/logo.svg" />
-      <h1 className="mt-5 text-sm font-bold ">Leaf Envionnmental</h1>
-      <div>
-        <ul className="pl-14 flex justify-center gap-5 mt-5 text-base cursor-pointer font-semibold">
-          <li className="hover:bg-blue-500">Home</li>
-          <li className="hover:bg-blue-500">About</li>
-          <li className="hover:bg-blue-500">Our Mission</li>
-          <li className="hover:bg-blue-500">Pages</li>
-          <li className="hover:bg-blue-500">Support</li>
-        </ul>
-      </div >
-      
-      <button className=" gap-1 flex ml-6 mt-5 bg-yellow-400  text-sm cursor-pointer font-semibold pl-3 pr-3 pb-1 pt-1 hover:bg-blue-500">
-        <img className="h-5 w-5 " src="currencyexchange.svg" />
-        Contibuet</button>
-      <button className=" flex gap-1 ml-2 mt-5 bg-yellow-400 text-sm cursor-pointer font-semibold pl-3 pr-3 pb-1 pt-1 hover:bg-blue-500">
-      <img className="h-5 w-5 " src="input_FILL0_wght400_GRAD0_opsz48.svg" />
-        Sign In</button>
-      
+    <div className=" container w-full h-full mx-auto my-4">
+      {/* header */}
+      <div className="w-full h-full flex items-center justify-between">
+        <div className="flex items-end justify-center gap-5">
+          <img className="h-[50px] w-[50px]" src="/logo.svg" />
+          <h1 className="text-sm font-bold">Leaf Envionnmental</h1></div>
+        <div>
+          <ul className="flex justify-center gap-x-4 text-base cursor-pointer font-semibold">
+            <li className="hover:bg-blue-500">Home</li>
+            <li className="hover:bg-blue-500">About</li>
+            <li className="hover:bg-blue-500">Our Mission</li>
+            <li className="hover:bg-blue-500">Pages</li>
+            <li className="hover:bg-blue-500">Support</li>
+          </ul>
+        </div>
+        <div className="flex gap-5">
+          <button className=" h-8 rounded-full gap-1 flex bg-yellow-400  text-sm cursor-pointer font-semibold px-3 py-1 hover:bg-blue-500">
+            <img className="h-5 w-5 " src="currencyexchange.svg" />
+            Contibuet</button>
+          <button className=" h-8 rounded-full flex gap-1 bg-yellow-400 text-sm cursor-pointer font-semibold px-3 py-1 hover:bg-blue-500">
+            <img className="h-5 w-5 " src="input_FILL0_wght400_GRAD0_opsz48.svg" />
+            Sign In</button>
+        </div>
       </div>
-      <div className="pl-14 pr-14 w-full h-full w-max h-max items-center mr-auto ml-auto mt-12 flex justify-center justify-between gap-5 ">
+      {/* b */}
+      <div className="items-center flex justify-center  gap-9 ">
         <div className="">
-          <h1 className="text-5xl font-bold pb-10">We safeguard</h1>
-          <h1 className="text-3xl font-bold pb-10">The environment.</h1>
-          <p className="w-96 font-bold pb-">Over the past several decades, as the world has increasingly warmed,
-             so has its potential to burn.</p>
+          <h1 className="text-6xl font-bold pb-16">We safeguard</h1>
+          <h1 className="text-4xl font-bold pb-16">The environment.</h1>
+          <p className="w-96  font-bold text-lx ">Over the past several decades, as the world has increasingly warmed,
+            so has its potential to burn.</p>
         </div>
-        <div><img className="w-full h-[400px]" src="./forest.svg"></img></div> 
+        <div><img className="w-[900px] h-[800px]" src="./forest.svg"></img></div>
       </div>
-      <div className="flex pl-14 pr-10 pb-10 gap-5">
-      <button className=" gap-1 flex ml-6 mt-5 bg-gray-300  text-sm cursor-pointer font-semibold pl-3 pr-3 pb-1 pt-1 hover:bg-blue-400">
-        <img className="h-5 w-5 " src="local_library_FILL0_wght400_GRAD0_opsz48.svg" />
-        Contibuet</button>
-      <button className=" flex gap-1 ml-2 mt-5 bg-gray-300 text-sm cursor-pointer font-semibold pl-3 pr-3 pb-1 pt-1 hover:bg-blue-400">
-      <img className="h-5 w-5 " src="sim_card_download_FILL0_wght400_GRAD0_opsz48.svg" />
-        Sign In</button>
-        </div>
+      <div className="flex justify-end gap-5">
+        <button className=" rounded-full gap-1 flex ml-6 mt-1 bg-gray-300  text-sm cursor-pointer font-semibold pl-3 pr-3 pb-1 pt-1 hover:bg-blue-400">
+          <img className="h-5 w-5 " src="local_library_FILL0_wght400_GRAD0_opsz48.svg" />
+          Our mission</button>
+        <button className=" rounded-full flex gap-1 ml-2 mt-1 bg-gray-300 text-sm cursor-pointer font-semibold pl-3 pr-3 pb-1 pt-1 hover:bg-blue-400">
+          <img className="h-5 w-5 " src="sim_card_download_FILL0_wght400_GRAD0_opsz48.svg" />
+          Donwload report</button>
+      </div>
     </div>
-    
+
   );
 }
 
